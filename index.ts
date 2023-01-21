77910d24da6da75a8ad70920eb2eea19c55ba3f2
Ariba-memon <aribaabdulqadir786@gmail.com>
2023-01-22 00:50:15 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,102 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import { log } from "console";
+
+
+
+
+
+
+
+
+const sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 3000)
+    })
+}
+async function welcome() {
+    let rainbowTitle = chalkAnimation.karaoke(" Welcome to Currency Converter How can i help you");
+    await sleep();
+    rainbowTitle.stop();
+}
+
+await welcome()
+function ready() {
+    console.log(' ');
+
+
+}
+
+ready()
+
+
+
+
+
+
+let convertions = {
+
+    "PKR": {
+        "USD": 260,
+        "DHARAM": 280,
+        "PKR": 1,
+
+    },
+    "USD": {
+        "DHARAM": 280,
+        "PKR": 10,
+        "USD": 1,
+    },
+
+
+    "DHARAM": {
+        "PKR": 70,
+        "USD": 10,
+        "DHARAM": 1,
+    },
+}
+
+
+
+let answers: {
+
+    from: "PKR" | "USD" | "DHARAM"
+
+    to: "PKR" | "USD" | "DHARAM"
+    "amount": number
+} = await inquirer.prompt([{
+
+    type: "list",
+    name: "from",
+    choices: ["PKR", "USD", "DHARAM"],
+    message: "Currency you have:"
+},
+{
+    type: "list",
+    name: "to",
+    choices: ["PKR", "USD", "DHARAM"],
+    message: "Currency you Want:"
+},
+{
+    type: "number",
+    name: "amount",
+
+    message: "Enter Amount You want:"
+}
+
+
+])
+const { from, to, amount } = answers
+// let answer = answers.amount = Math.floor(2)
+
+if (answers.from && answers.to && answers.amount) {
+    let convertedvalue = convertions
+    [from][to] * amount;
+
+    console.log(`your's curency value is${from}to${to} is ${convertedvalue}`)
+
+} else {
+    "Do not any amount"
+}
